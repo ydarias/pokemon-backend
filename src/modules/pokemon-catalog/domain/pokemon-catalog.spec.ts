c1d3803af991881223f368b2ed0a7dcc8912b230
Yeray Darias <ydarias@gmail.com>
2022-01-24 17:42:24 +0100

    Extracted hardcoded test values to utility class

--- src/modules/pokemon-catalog/domain/pokemon-catalog.spec.ts
+++ src/modules/pokemon-catalog/domain/pokemon-catalog.spec.ts
@@ -1,73 +1,12 @@
 import { ForQueryingPokemons } from './for-querying-pokemons';
 import { PokemonCatalog } from './pokemon-catalog';
-import { Pokemon } from './models';
+import { MockedPokemons } from '../../../utils/tests/pokemons';
 
 describe('A Pokemon Catalog', () => {
-  const pikachu: Pokemon = {
-    id: '025',
-    name: 'Pikachu',
-    classification: 'Mouse Pokémon',
-    types: ['Electric'],
-    resistant: ['Electric', 'Flying', 'Steel'],
-    weaknesses: ['Ground'],
-    weight: {
-      minimum: 5.25,
-      maximum: 6.75,
-      unit: 'kg',
-    },
-    height: {
-      minimum: 0.35,
-      maximum: 0.45,
-      unit: 'm',
-    },
-    fleeRate: 0.1,
-    evolutionRequirements: {
-      amount: 50,
-      name: 'Pikachu candies',
-    },
-    evolutions: [
-      {
-        id: '026',
-        name: 'Raichu',
-      },
-    ],
-    maxCP: 777,
-    maxHP: 887,
-    attacks: {
-      fast: [
-        {
-          name: 'Quick Attack',
-          type: 'Normal',
-          damage: 10,
-        },
-        {
-          name: 'Thunder Shock',
-          type: 'Electric',
-          damage: 5,
-        },
-      ],
-      special: [
-        {
-          name: 'Discharge',
-          type: 'Electric',
-          damage: 35,
-        },
-        {
-          name: 'Thunder',
-          type: 'Electric',
-          damage: 100,
-        },
-        {
-          name: 'Thunderbolt',
-          type: 'Electric',
-          damage: 55,
-        },
-      ],
-    },
-  };
-
   it('Gets a pokemon given its ID', () => {
     const pokemonCatalog: ForQueryingPokemons = new PokemonCatalog();
-    expect(pokemonCatalog.getPokemonByItsID('025')).toStrictEqual(pikachu);
+    expect(pokemonCatalog.getPokemonByItsID('025')).toStrictEqual(
+      MockedPokemons.pikachu(),
+    );
   });
 });
--- src/modules/pokemon-catalog/domain/pokemon-catalog.ts
+++ src/modules/pokemon-catalog/domain/pokemon-catalog.ts
@@ -1,69 +1,9 @@
 import { ForQueryingPokemons } from './for-querying-pokemons';
 import { Pokemon } from './models';
+import { MockedPokemons } from '../../../utils/tests/pokemons';
 
 export class PokemonCatalog implements ForQueryingPokemons {
   getPokemonByItsID(id: string): Pokemon {
-    return {
-      id: '025',
-      name: 'Pikachu',
-      classification: 'Mouse Pokémon',
-      types: ['Electric'],
-      resistant: ['Electric', 'Flying', 'Steel'],
-      weaknesses: ['Ground'],
-      weight: {
-        minimum: 5.25,
-        maximum: 6.75,
-        unit: 'kg',
-      },
-      height: {
-        minimum: 0.35,
-        maximum: 0.45,
-        unit: 'm',
-      },
-      fleeRate: 0.1,
-      evolutionRequirements: {
-        amount: 50,
-        name: 'Pikachu candies',
-      },
-      evolutions: [
-        {
-          id: '026',
-          name: 'Raichu',
-        },
-      ],
-      maxCP: 777,
-      maxHP: 887,
-      attacks: {
-        fast: [
-          {
-            name: 'Quick Attack',
-            type: 'Normal',
-            damage: 10,
-          },
-          {
-            name: 'Thunder Shock',
-            type: 'Electric',
-            damage: 5,
-          },
-        ],
-        special: [
-          {
-            name: 'Discharge',
-            type: 'Electric',
-            damage: 35,
-          },
-          {
-            name: 'Thunder',
-            type: 'Electric',
-            damage: 100,
-          },
-          {
-            name: 'Thunderbolt',
-            type: 'Electric',
-            damage: 55,
-          },
-        ],
-      },
-    };
+    return MockedPokemons.pikachu();
   }
 }
